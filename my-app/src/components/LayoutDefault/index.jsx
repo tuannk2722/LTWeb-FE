2baca7dbe75d548c14d0109091ee404c7bea60fc
tuannk1 <tuannk1@hblab.vn>
2026-02-21 18:44:11 +0700

    code layout default

--- my-app/src/components/LayoutDefault/index.jsx
+++ my-app/src/components/LayoutDefault/index.jsx
@@ -1,11 +1,61 @@
-import { Outlet } from "react-router-dom";
+import { Layout, Menu } from "antd";
+import { Link, Outlet, useLocation } from "react-router-dom";
+import { LoginOutlined, LogoutOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import logo from "../../image/logo.jpg";
 
-function LayoutDetaul() {
+const { Header, Content, Footer } = Layout;
+
+function LayoutDefault() {
+    const [current, setCurrent] = useState('home')
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname === "/"){
+            setCurrent("home");
+        } else {
+            setCurrent(location.pathname.replace("/", ""))
+        }
+    }, [location.pathname])
+
+    const items = [
+        {
+            key: "login",
+            label: <Link to="login">Đăng nhập</Link>,
+            icon: <LoginOutlined />, 
+            className: "menu__login",
+        },
+        {
+            key: "register",
+            icon: <LogoutOutlined />,
+            label: <Link to="register">Đăng ký</Link>
+        }
+    ]
+
     return (
         <>
-            LayoutDetaul
-            <Outlet />
+            <Layout>
+                <Header style={{display: "flex", alignItems: "center", backgroundColor: "#fff", position: "fixed", zIndex: 2, width: "100%"}}>
+                    <Link to="/" className="menu__logo">
+                        <img src={logo} alt="Logo" />
+                    </Link>
+                    <Menu 
+                        items={items}
+                        mode="horizontal"
+                        selectedKeys={[current]}
+                        style={{ flex: 1, minWidth: 0 }}
+                    />
+                </Header>
+
+                <Content style={{marginTop: 64, minHeight: "80vh"}}>
+                    <Outlet />
+                </Content>
+
+                <Footer style={{display: "flex", justifyContent: "center"}}>
+                    Copyright @by Tuan NK
+                </Footer>
+            </Layout>
         </>
     )
 }
-export default LayoutDetaul;
+export default LayoutDefault;
